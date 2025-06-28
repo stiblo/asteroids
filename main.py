d75849db1bbf3318169f44e8cdaaf1ed20fc3004
Amir Zahirovic <amirzahirovic0@gmail.com>
2025-06-28 07:45:42 +0200

    CH2L5: there is actually a triange on the screen

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     print(f"Starting Asteroids!\nScreen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
     pygame.init()
     clock_object = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
     while True:
@@ -12,6 +14,7 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
         dt = clock_object.tick(60) / 1000
 
