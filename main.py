435f6b27dd1746b0f67baedb84ec92f7cf2f3561
Amir Zahirovic <amirzahirovic0@gmail.com>
2025-06-28 07:36:18 +0200

    CH2L4: added the tick and assigned value to the dt variable, not sure if i have done that properly

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from constants import *
 def main():
     print(f"Starting Asteroids!\nScreen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
     pygame.init()
+    clock_object = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
     while True:
         for event in pygame.event.get():
@@ -11,6 +13,7 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+        dt = clock_object.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
